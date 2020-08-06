36f16cce0bbba5160ffa5f64ba5c246c70d8e511
Christopher Tubbs <ctubbsii@apache.org>
2020-08-06 07:12:29 -0400

    Update messages; check protected before removal

--- default-branch-updater.py
+++ default-branch-updater.py
@@ -35,17 +35,21 @@ org = Github(token).get_organization(org_name)
 
 for repo in org.get_repos(type='public'):
     if repo.name.startswith(prefix):
-        print(f'Updating {repo.name}...')
-        print(f'    -> Changing default branch to {new_default_branch}...')
+        print(f'Updating "{org_name}/{repo.name}"...')
+        print(f'    -> Changing default branch to "{new_default_branch}"...')
         repo.edit(default_branch = new_default_branch)
         # this branch protection only stops force pushes and deletions, nothing else
-        print(f'    -> Setting branch protection on {new_default_branch}...')
+        print(f'    -> Setting branch protection on "{new_default_branch}"...')
         repo.get_branch(new_default_branch).edit_protection()
         # this may or may not remove the branch protection, depending on whether
         # branch protection rule is using a pattern or an exact name; if it has an
         # exact name that matches the old default branch, all protections will be
         # removed, not just the deletion and force push restrictions
-        print(f'    -> Removing branch protection from {old_default_branch}...')
-        repo.get_branch(old_default_branch).remove_protection()
+        branch = repo.get_branch(old_default_branch)
+        if branch.protected:
+            print(f'    -> Removing branch protection from "{old_default_branch}"...')
+            repo.get_branch(old_default_branch).remove_protection()
+        else:
+            print(f'    -> (Skipped) Branch protection not set on "{old_default_branch}"')
         print('    Done.')
 
